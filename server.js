fb172bb5e5082c8f0f30c0a3b020f55447d66eb7
Milos Kallai <miloskallai@Miloss-Mac-mini.local>
2019-01-17 17:01:43 +0100

    (add) express as dependency

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-const PORT = 8080;
-
-app.listen(PORT, () => {
-  console.log(`The server is up and running on port: ${PORT}`);
-});
